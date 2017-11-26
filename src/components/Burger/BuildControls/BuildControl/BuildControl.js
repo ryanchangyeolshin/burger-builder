12cbb773802d7926fa8364283d810ddb4e3092ce
Ryan Shin <changyeolshin@gmail.com>
2017-11-25 23:17:04 -0800

    Provided onClick functions to the BuildControl component and added transformedIngredients to the Burger component. #2

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import classes from './BuildControl.css'
 
-const BuildControl = ({ label }) => (
+const BuildControl = ({ label, added, removed, disabled }) => (
   <div className={classes.BuildControl}>
     <div className={classes.Label}>{label}</div>
-    <button className={classes.Less}>Less</button>
-    <button className={classes.More}>More</button>
+    <button
+      className={classes.Less}
+      onClick={removed}
+      disabled={disabled}>Less</button>
+    <button
+      className={classes.More}
+      onClick={added}>More</button>
   </div>
 )
 
--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,12 +2,23 @@ import React from 'react'
 import BurgerIngredients from './BurgerIngredients/BurgerIngredients'
 import classes from './Burger.css'
 
-const Burger = props => {
+const Burger = ({ ingredients }) => {
+  let transformedIngredients = Object.keys(ingredients)
+    .map(ingredient => [...Array(ingredients[ingredient])].map((_, i) => {
+      return <BurgerIngredients key={ingredient + i} type={ingredient} />
+    }))
+    .reduce((acc, value) => {
+      return acc.concat(value)
+    }, [])
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>
+  }
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredients type="bread-top" />
-      <BurgerIngredients type="cheese" />
-      <BurgerIngredients type="meat" />
+      {transformedIngredients}
       <BurgerIngredients type="bread-bottom" />
     </div>
   )
