bd119655a69d63eb1e6e9594184a1289bb24199c
Ryan Shin <changyeolshin@gmail.com>
2017-11-25 18:41:48 -0800

    Created the Layout component. #1

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Aux from '../../HOC/Aux'
+import classes from './Layout.css'
 
 const Layout = props => {
   return (
     <Aux>
       <div>Toolbar, SideDrawer, BackDrop</div>
-      <main>
+      <main className={classes.Content}>
         {props.children}
       </main>
     </Aux>
