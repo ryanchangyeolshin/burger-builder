573bf6be8c95346f292baba773379647dea43596
Ryan Shin <changyeolshin@gmail.com>
2017-11-25 18:55:34 -0800

    Added styling and added the BurgerIngredients component for bun, meat, and cheese. #1

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import BurgerIngredients from './BurgerIngredients/BurgerIngredients'
+import classes from './Burger.css'
 
 const Burger = props => {
   return (
-    
+    <div className={classes.Burger}>
+      <BurgerIngredients type="bread-top" />
+      <BurgerIngredients type="cheese" />
+      <BurgerIngredients type="meat" />
+      <BurgerIngredients type="bread-bottom" />
+    </div>
   )
 }
 
