8b688509f1438517ece2dc0faa0cf23a9b5b1d38
Ryan Shin <changyeolshin@gmail.com>
2017-11-27 20:09:50 -0800

    Inside the purchaseContinueHandler, POST request functionality is created using the firebase URL. #11

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 import Modal from '../../components/UI/Modal/Modal'
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
+import axios from '../../axios-orders'
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -76,7 +77,24 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    alert('You continue!')
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Ryan Shin',
+        age: 25,
+        address: {
+          street: 'Test Street',
+          zipCode: '12345',
+          country: 'South Korea'
+        },
+        email: 'ryanshin1992@gmail.com'
+      },
+      email: 'ryanshin1992@gmail.com'
+    }
+    axios.post('/orders.json', order)
+      .then(response => console.log(response))
+      .catch(err => console.log(err))
   }
 
   render() {
