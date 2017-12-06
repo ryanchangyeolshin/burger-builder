dd0c84efa0cd90318051976f80d65e30c54bc5cf
Ryan Shin <changyeolshin@gmail.com>
2017-12-06 14:08:09 -0800

    Moved the firebase POST functionality to the ContactData container. #13

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -86,29 +86,16 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    this.setState({ loading: true })
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Ryan Shin',
-        age: 25,
-        address: {
-          street: 'Test Street',
-          zipCode: '12345',
-          country: 'South Korea'
-        },
-        email: 'ryanshin1992@gmail.com'
-      },
-      email: 'ryanshin1992@gmail.com'
+    const queryParams = []
+    for (let i in this.state.ingredients) {
+      queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
     }
-    axios.post('/orders.json', order)
-      .then(response => {
-        this.setState({ loading: false, purchasing: false })
-      })
-      .catch(err => {
-        this.setState({ loading: false, purchasing: false })
-      })
+    queryParams.push('price=' + this.state.totalPrice)
+    const queryString = queryParams.join('&')
+    this.props.history.push({
+      pathname: '/checkout',
+      search: '?' + queryString
+    })
   }
 
   render() {
