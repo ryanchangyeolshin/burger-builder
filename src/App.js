9dc8c99d795513c3dcf60ca981d9e81e77665a2c
Ryan Shin <changyeolshin@gmail.com>
2017-12-31 14:22:50 -0800

    Provided routing to the App container. #13

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import { Switch, Route } from 'react-router-dom'
 import Layout from './HOC/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './containers/Checkout/Checkout'
+import Orders from './containers/Orders/Orders'
 import './App.css';
 
 class App extends Component {
-
   render() {
     return (
       <div>
         <Layout>
           <Switch>
             <Route path="/checkout" component={Checkout} />
+            <Route path="/orders" component={Orders} />
             <Route path="/" exact component={BurgerBuilder} />
           </Switch>
         </Layout>
