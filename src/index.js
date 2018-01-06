18026edab04a58a703994b1f6de6e356cad91a83
Ryan Shin <changyeolshin@gmail.com>
2018-01-06 13:40:05 -0800

    Connected redux store to the burger builder application. #17

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App'
-import reducer from './store/reducer'
+import burgerBuilderReducer from './store/reducers/burgerBuilder'
 import registerServiceWorker from './registerServiceWorker'
 
-const store = createStore(reducer)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(burgerBuilderReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 const app = (
   <Provider store={store}>
