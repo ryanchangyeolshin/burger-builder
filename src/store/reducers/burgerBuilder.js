bb69d02731e34d12f8ccd091eba4e2cf5b304b4d
Ryan Shin <changyeolshin@gmail.com>
2018-01-06 13:41:19 -0800

    Created the burgerBuilder reducer. #17

---
+++ src/store/reducers/burgerBuilder.js
@@ -0,0 +1,45 @@
+import { ADD_INGREDIENT, REMOVE_INGREDIENT } from '../actions/actionTypes'
+
+const initialState = {
+  ingredients: {
+    salad: 0,
+    bacon: 0,
+    cheese: 0,
+    meat: 0
+  },
+  totalPrice: 4
+}
+
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7
+}
+
+const burgerBuilderReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_INGREDIENT:
+      return {
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+        },
+        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+      }
+    case REMOVE_INGREDIENT:
+      return {
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+        },
+        totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
+      }
+    default:
+      return state
+  }
+}
+
+export default burgerBuilderReducer
