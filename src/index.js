e31befd507ec6f8293d3f47ec9be93618db2a9cf
Ryan Shin <changyeolshin@gmail.com>
2018-01-06 21:23:56 -0800

    Combined the burgerBuilder and orders reducers. #17

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware, compose } from 'redux'
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App'
 import burgerBuilderReducer from './store/reducers/burgerBuilder'
+import ordersReducer from './store/reducers/order'
 import registerServiceWorker from './registerServiceWorker'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const rootReducer = combineReducers({
+  burgerBuilder: burgerBuilderReducer,
+  orders: ordersReducer
+})
 
-const store = createStore(burgerBuilderReducer, composeEnhancers(applyMiddleware(thunk)))
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 const app = (
   <Provider store={store}>
