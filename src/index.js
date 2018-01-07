552c884df7584389632be8b227dff5077109a26b
Ryan Shin <changyeolshin@gmail.com>
2018-01-07 14:43:05 -0800

    Linked the authReducer to the rootReducer. #19

--- src/index.js
+++ src/index.js
@@ -8,12 +8,14 @@ import './index.css'
 import App from './App'
 import burgerBuilderReducer from './store/reducers/burgerBuilder'
 import ordersReducer from './store/reducers/order'
+import authReducer from './store/reducers/auth'
 import registerServiceWorker from './registerServiceWorker'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,
-  orders: ordersReducer
+  orders: ordersReducer,
+  auth: authReducer
 })
 
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
