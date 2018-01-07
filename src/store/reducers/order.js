161f77e1d784c5d755c6e7fb086666780ae002a2
Ryan Shin <changyeolshin@gmail.com>
2018-01-06 23:05:25 -0800

    Created a reducer for the PURCHASE_INIT action type on orders.js #17

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -1,12 +1,18 @@
-import { PURCHASE_BURGER_START, PURCHASE_BURGER_SUCCESS, PURCHASE_BURGER_FAILED } from '../actions/actionTypes'
+import { PURCHASE_INIT, PURCHASE_BURGER_START, PURCHASE_BURGER_SUCCESS, PURCHASE_BURGER_FAILED } from '../actions/actionTypes'
 
 const initialState = {
   orders: [],
-  loading: false
+  loading: false,
+  purchased: false
 }
 
 const ordersReducer = (state = initialState, action) => {
   switch (action.type) {
+    case PURCHASE_INIT:
+      return {
+        ...state,
+        purchased: false
+      }
     case PURCHASE_BURGER_START:
       return {
         ...state,
@@ -20,7 +26,8 @@ const ordersReducer = (state = initialState, action) => {
       return {
         ...state,
         loading: false,
-        orders: state.orders.concat(newOrder)
+        orders: state.orders.concat(newOrder),
+        purchased: true
       }
     case PURCHASE_BURGER_FAILED:
       return {
