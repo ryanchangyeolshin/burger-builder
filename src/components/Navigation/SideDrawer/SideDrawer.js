9b0dc5c19980727d88c3f0973b84815745e4edcf
Ryan Shin <changyeolshin@gmail.com>
2018-01-07 22:16:18 -0800

    Passed isAuthenticated property as a prop to the NavigationItems component. #19

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -18,7 +18,7 @@ const SideDrawer = (props) => {
           <Logo />
         </div>
         <nav>
-          <NavigationItems />
+          <NavigationItems isAuthenticated={props.isAuth} />
         </nav>
       </div>
     </Aux>
