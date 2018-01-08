419aca8523334951079710e90b8019c851295080
Ryan Shin <changyeolshin@gmail.com>
2018-01-08 14:25:28 -0800

    Mapped the setAuthRedirectPath to props. #19

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
-import { auth } from '../../store/actions/index'
+import { auth, setAuthRedirectPath } from '../../store/actions/index'
 import Input from '../../components/UI/Input/Input'
 import Button from '../../components/UI/Button/Button'
 import Spinner from '../../components/UI/Spinner/Spinner'
@@ -40,6 +40,12 @@ class Auth extends Component {
       }
     },
     isSignup: true
+  }
+
+  componentDidMount() {
+    if (this.props.building && this.props.authRedirect !== '/') {
+      this.props.onSetAuthRedirectPath()
+    }
   }
 
   checkValidity = (value, rules) => {
@@ -133,7 +139,7 @@ class Auth extends Component {
 
     let authRedirect = null
     if (this.props.isAuthenticated) {
-      authRedirect = <Redirect to="/" />
+      authRedirect = <Redirect to={this.props.authRedirectPath} />
     }
 
     return (
@@ -158,13 +164,16 @@ const mapStateToProps = state => {
   return {
     loading: state.auth.loading,
     error: state.auth.error,
-    isAuthenticated: state.auth.token !== null
+    isAuthenticated: state.auth.token !== null,
+    building: state.burgerBuilder.building,
+    authRedirectPath: state.auth.authRedirectPath
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
-    onAuth: (email, password, isSignup) => dispatch(auth(email, password, isSignup))
+    onAuth: (email, password, isSignup) => dispatch(auth(email, password, isSignup)),
+    onSetAuthRedirectPath: () => dispatch(setAuthRedirectPath('/'))
   }
 }
 
