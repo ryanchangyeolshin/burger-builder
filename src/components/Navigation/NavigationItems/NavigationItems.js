8e21042cdbbb6d534a46829a104c5d5b3f081dcb
Ryan Shin <changyeolshin@gmail.com>
2018-01-07 22:41:22 -0800

    Created functionality for the Orders navigation to disappear if the user is not authenticated. #19

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -5,7 +5,7 @@ import classes from './NavigationItems.css'
 const NavigationItems = (props) => (
   <ul className={classes.NavigationItems}>
     <NavigationItem link="/" exact>Burger Builder</NavigationItem>
-    <NavigationItem link="/orders">Orders</NavigationItem>
+    {props.isAuthenticated ? <NavigationItem link="/orders">Orders</NavigationItem> : null}
     {!props.isAuthenticated
       ? <NavigationItem link="/auth">Authenticate</NavigationItem>
       : <NavigationItem link="/logout">Logout</NavigationItem>
