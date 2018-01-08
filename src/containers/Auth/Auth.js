bb6fbb925d57f515b78f137dd15cd9b0b9e20633
Ryan Shin <changyeolshin@gmail.com>
2018-01-07 19:50:17 -0800

    Provided functionality to add a Spinner component if the page is loading. #19

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import { auth } from '../../store/actions/index'
 import Input from '../../components/UI/Input/Input'
 import Button from '../../components/UI/Button/Button'
+import Spinner from '../../components/UI/Spinner/Spinner'
 import classes from './Auth.css'
 
 class Auth extends Component {
@@ -103,7 +104,7 @@ class Auth extends Component {
       })
     }
 
-    const form = formElementsArray.map(formElement => {
+    let form = formElementsArray.map(formElement => {
       return (
         <Input
           key={formElement.id}
@@ -118,8 +119,20 @@ class Auth extends Component {
       )
     })
 
+    if (this.props.loading) {
+      form = <Spinner />
+    }
+
+    let errorMessage = null
+    if (this.props.error) {
+      errorMessage = (
+        <p>{this.props.error.message}</p>
+      )
+    }
+
     return (
       <div className={classes.Auth}>
+        {errorMessage}
         <form onSubmit={this.onSubmitHandler}>
           {form}
           <Button btnType="Success">SUBMIT</Button>
@@ -134,10 +147,17 @@ class Auth extends Component {
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    loading: state.auth.loading,
+    error: state.auth.error
+  }
+}
+
 const mapDispatchToProps = dispatch => {
   return {
     onAuth: (email, password, isSignup) => dispatch(auth(email, password, isSignup))
   }
 }
 
-export default connect(null, mapDispatchToProps)(Auth)
+export default connect(mapStateToProps, mapDispatchToProps)(Auth)
