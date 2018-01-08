cfc0dae7e3f21b8395dd8620b7a3030f33daea04
Ryan Shin <changyeolshin@gmail.com>
2018-01-07 22:15:29 -0800

    Mapped the isAuthenticated state to props. #19

--- src/HOC/Layout/Layout.js
+++ src/HOC/Layout/Layout.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import Aux from '../Aux/Aux'
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar'
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer'
@@ -22,8 +23,11 @@ class Layout extends Component {
   render() {
     return (
       <Aux>
-        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <Toolbar
+          isAuth={this.props.isAuthenticated}
+          drawerToggleClicked={this.sideDrawerToggleHandler} />
         <SideDrawer
+          isAuth={this.props.isAuthenticated}
           open={this.state.showSideDrawer}
           closed={this.sideDrawerClosedHandler} />
         <div>Toolbar, SideDrawer, BackDrop</div>
@@ -35,4 +39,10 @@ class Layout extends Component {
   }
 }
 
-export default Layout
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  }
+}
+
+export default connect(mapStateToProps, null)(Layout)
