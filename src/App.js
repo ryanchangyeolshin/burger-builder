aca660d8004d82e410804dad69fe43682b91368e
Ryan Shin <changyeolshin@gmail.com>
2018-01-08 22:24:50 -0800

    Added the /auth path when the user is authenticated. #19

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ class App extends Component {
         <Switch>
           <Route path="/checkout" component={Checkout} />
           <Route path="/orders" component={Orders} />
+          <Route path="/auth" component={Auth} />
           <Route path="/logout" component={Logout} />
           <Route path="/" exact component={BurgerBuilder} />
         </Switch>
