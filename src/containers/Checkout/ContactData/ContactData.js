82c130e140550902588444c2aac6e4607e802be2
Ryan Shin <changyeolshin@gmail.com>
2018-01-08 22:24:14 -0800

    Mapped userId to props inside the ContactData container. #19

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -103,7 +103,8 @@ class ContactData extends Component {
     const order = {
       ingredients: this.props.ingredients,
       price: this.props.totalPrice,
-      orderData: formData
+      orderData: formData,
+      userId: this.props.userId
     }
     this.props.onOrderBurger(order, this.props.token)
   }
@@ -186,7 +187,8 @@ const mapStateToProps = state => {
     ingredients: state.burgerBuilder.ingredients,
     totalPrice: state.burgerBuilder.totalPrice,
     loading: state.orders.loading,
-    token: state.auth.token
+    token: state.auth.token,
+    userId: state.auth.userId
   }
 }
 
