074668ef114e22eb488624151175ee43df70dbb6
Ryan Shin <changyeolshin@gmail.com>
2018-01-08 22:22:21 -0800

    Mapped userId to props inside the Orders container. #19

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -9,7 +9,7 @@ import { fetchOrders } from '../../store/actions/index'
 class Orders extends Component {
 
   componentDidMount() {
-    this.props.onFetchOrders(this.props.token)
+    this.props.onFetchOrders(this.props.token, this.props.userId)
   }
 
   render() {
@@ -34,13 +34,14 @@ const mapStateToProps = state => {
   return {
     orders: state.orders.orders,
     loading: state.orders.loading,
-    token: state.auth.token
+    token: state.auth.token,
+    userId: state.auth.userId
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
-    onFetchOrders: token => dispatch(fetchOrders(token))
+    onFetchOrders: (token, userId) => dispatch(fetchOrders(token, userId))
   }
 }
 
