470e59418e73115e272c3fbcaaa266c476170f81
Ryan Shin <changyeolshin@gmail.com>
2018-01-08 22:23:14 -0800

    Added query params to the fetchOrders action creator. #19

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -67,9 +67,10 @@ export const fetchOrdersFail = error => {
   }
 }
 
-export const fetchOrders = token => dispatch => {
+export const fetchOrders = (token, userId) => dispatch => {
   dispatch(fetchOrdersStart())
-  axios.get('/orders.json?auth=' + token)
+  const queryParams = `?auth=${token}&orderBy="userId"&equalTo="${userId}"`
+  axios.get('/orders.json' + queryParams)
     .then(res => {
       const fetchedOrders = []
       for (let key in res.data) {
